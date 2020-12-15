0756510c3538f626a319fc0a32c0f7d48d447468
Intimaria <intimaria@gmail.com>
2020-12-15 02:21:27 +0000

    Add new file

---
+++ reverse_string.sh
@@ -0,0 +1,17 @@
+#!/usr/local/bin/bash
+# Reverse a string
+
+# test for input 
+if [ $# -lt 1]; then
+    echo "Usage: $( basename $0 ) <string>" 
+		exit 1
+fi
+
+function print_backwards()
+{
+   rev <<< "$1"
+   return 0
+}
+
+print_backwards "$@"
+exit 0
